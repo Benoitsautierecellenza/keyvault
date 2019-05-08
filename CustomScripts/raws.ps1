1312554cc5f69c4bd4fb93fada1d0360d69436a2
benoit.Sautiere@cellenza.com <benoit.Sautiere@cellenza.com>
2019-05-08 18:00:23 +0200

    Customscripts

---
+++ CustomScripts/raws.ps1
@@ -0,0 +1,35 @@
+#
+# Initialize all new disks (not yet initialized)
+#
+$disk = Get-Disk | Where-Object {$_.partitionStyle -eq 'RAW'}
+If([string]::IsNullOrEmpty($disk)-eq $false)
+{
+    #
+    # Initialize Disks in RAW mode
+    #
+    Initialize-Disk -InputObject $disk -PartitionStyle MBR
+    #
+    # Create partitions
+    #
+    $disk | New-Partition -AssignDriveLetter -UseMaximumSize
+    $partitions = get-partition |  Where-Object {$_.disknumber -ge 2}
+    #
+    # Format all volumes
+    #
+format-volume -Partition $partitions -FileSystem NTFS
+}
+#
+# Install Chocolatey
+#
+Set-ExecutionPolicy Bypass -Scope Process -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#
+# Create a list of packages to be processed
+#
+#$Packages = 'git'
+$Packages = $null
+#
+# Process eack package for installation
+#
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
